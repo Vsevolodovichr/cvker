3f29807dca7f6889473cc4a96822eb30505c1baf
Roman Shkurenko <130462736+Vsevolodovichr@users.noreply.github.com>
2025-05-22 21:33:58 +0000

    333

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function setLang(lang) {
-    document.querySelectorAll('[data-lang-uk]').forEach(el => {
-        el.textContent = lang === 'uk' ? el.dataset.langUk : el.dataset.langEn;
-    });
-    localStorage.setItem('lang', lang);
-}
-
-function generateCV() {
-    const form = document.forms['cv-form'];
-    document.getElementById('cv-name').innerText = form.name.value;
-    document.getElementById('cv-title').innerText = form.title.value;
-    document.getElementById('cv-about').innerText = form.about.value;
-}
 function detectLanguage() {
     let lang = navigator.language || navigator.userLanguage;
 
@@ -34,6 +21,20 @@ function updateLangButton(currentLang) {
 }
 
 window.onload = detectLanguage;
+
+function setLang(lang) {
+    document.querySelectorAll('[data-lang-uk]').forEach(el => {
+        el.textContent = lang === 'uk' ? el.dataset.langUk : el.dataset.langEn;
+    });
+    localStorage.setItem('lang', lang);
+}
+
+function generateCV() {
+    const form = document.forms['cv-form'];
+    document.getElementById('cv-name').innerText = form.name.value;
+    document.getElementById('cv-title').innerText = form.title.value;
+    document.getElementById('cv-about').innerText = form.about.value;
+}
 
 function downloadPDF() {
     const element = document.getElementById('cv-template');
